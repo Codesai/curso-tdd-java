f547f3b5a5172a1405a03d1ef94ab74ac43ed989
trikitrok <trikitrok@gmail.com>
2019-07-03 20:12:52 +0200

    adding pitest plugin and creating package

---
+++ katas-java/password-validator/src/test/java/password_validation/unit_tests/PasswordValidatorTest.java
@@ -0,0 +1,15 @@
+package password_validation.unit_tests;
+
+import org.junit.Test;
+import password_validation.PasswordValidator;
+
+import static org.junit.Assert.assertTrue;
+
+public class PasswordValidatorTest {
+
+	@Test
+	public void fix_me_and_rename() {
+		assertTrue(PasswordValidator.isValid("koko"));
+	}
+
+}
