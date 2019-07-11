04daf1e8eb03f0212b79e0210805dbcd7f8ff9fa
trikitrok <trikitrok@gmail.com>
2019-07-11 23:02:31 +0200

    fixing packages so mutation testing works

---
+++ katas-java/password-validator/src/test/java/password_validation/PasswordValidatorTest.java
@@ -0,0 +1,14 @@
+package password_validation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class PasswordValidatorTest {
+
+	@Test
+	public void fix_me_and_rename() {
+		assertFalse(true);
+	}
+
+}
