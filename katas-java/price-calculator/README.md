38173ab81a463616acd9e950b17185b43f0fe496
freyes <fran@codesai.com>
2026-06-04 11:23:53 +0100

    adding tdd demo

---
+++ katas-java/price-calculator/README.md
@@ -0,0 +1,3 @@
+## Calculating final price per unit of a product.
+
+[Requirements](https://gist.github.com/trikitrok/86a1ec1e13bcd6840a1529e2f2ebe8a8)
---
+++ katas-java/price-calculator/src/test/java/ProductTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProductTest {
+    
+    @Test
+    void canary_test(){
+       assertThat(true).isTrue(); 
+    }
+}
