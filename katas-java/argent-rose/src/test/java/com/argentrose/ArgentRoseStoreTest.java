f44ff020e2c961b51f2f4e9490732bce22542a41
trikitrok <trikitrok@gmail.com>
2026-06-06 17:32:24 +0200

    renamed test class and adjusted package structure

--- katas-java/argent-rose/src/test/java/com/argentrose/ArgentRoseStoreTest.java
+++ katas-java/argent-rose/src/test/java/com/argentrose/ArgentRoseStoreTest.java
@@ -1,8 +1,10 @@
+package com.argentrose;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-public class ArgentRoseTest {
+public class ArgentRoseStoreTest {
 
 	@Test
 	public void fix_me_and_rename_me() {
