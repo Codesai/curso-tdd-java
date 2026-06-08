363caac455161a33c57cd2e2ad9aae27d01c6835
trikitrok <trikitrok@gmail.com>
2026-06-08 18:27:43 +0200

    using assertJ

--- katas-java/pure-greeter/src/test/java/puregreeter/PureGreeterTest.java
+++ katas-java/pure-greeter/src/test/java/puregreeter/PureGreeterTest.java
@@ -3,44 +3,43 @@ package puregreeter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PureGreeterTest {
 
-  private PureGreeter pureGreeter;
+    private PureGreeter pureGreeter;
 
-  @BeforeEach
-  void setUp() {
-    pureGreeter = new PureGreeter();
-  }
+    @BeforeEach
+    void setUp() {
+        pureGreeter = new PureGreeter();
+    }
 
-  @Test
-  void greet_during_the_morning() {
-    String greeting = pureGreeter.greeter(8, "Pepe");
+    @Test
+    void greet_during_the_morning() {
+        String greeting = pureGreeter.greeter(8, "Pepe");
 
-    assertEquals("¡Buenos días Pepe!", greeting);
-  }
+        assertThat(greeting).isEqualTo("¡Buenos días Pepe!");
+    }
 
-  @Test
-  void greet_during_the_afternoon() {
-    String greeting = pureGreeter.greeter(15, "Pepe");
+    @Test
+    void greet_during_the_afternoon() {
+        String greeting = pureGreeter.greeter(15, "Pepe");
 
-    assertEquals("¡Buenas tardes Pepe!", greeting);
-  }
+        assertThat(greeting).isEqualTo("¡Buenas tardes Pepe!");
+    }
 
-  @Test
-  void greet_during_the_night() {
-    String greeting = pureGreeter.greeter(22, "Pepe");
+    @Test
+    void greet_during_the_night() {
+        String greeting = pureGreeter.greeter(22, "Pepe");
 
-    assertEquals("¡Buenas noches Pepe!", greeting);
-  }
+        assertThat(greeting).isEqualTo("¡Buenas noches Pepe!");
+    }
 
-  @Test
-  void greeting_hours_should_be_between_0_and_23() {
-    assertThrows(
-            IllegalArgumentException.class,
-            () -> pureGreeter.greeter(40, "Pepe")
-    );
-  }
+    @Test
+    void greeting_hours_should_be_between_0_and_23() {
+        assertThatThrownBy(
+                () -> pureGreeter.greeter(40, "Pepe")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
 }
