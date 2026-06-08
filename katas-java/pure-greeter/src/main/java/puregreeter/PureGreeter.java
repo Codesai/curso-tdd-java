d17b44093cd46b6c32d3c765be103a4c9b2c84d3
trikitrok <trikitrok@gmail.com>
2026-06-08 18:28:06 +0200

    removed redundant elses

--- katas-java/pure-greeter/src/main/java/puregreeter/PureGreeter.java
+++ katas-java/pure-greeter/src/main/java/puregreeter/PureGreeter.java
@@ -2,15 +2,18 @@ package puregreeter;
 
 public class PureGreeter {
     public String greeter(int currentHour, String name) {
-        if (currentHour < 0 || currentHour > 23 ) {
-           throw new IllegalArgumentException("Hour must be between 0 and 23"); 
+        if (currentHour < 0 || currentHour > 23) {
+            throw new IllegalArgumentException("Hour must be between 0 and 23");
         }
+
         if (currentHour >= 6 && currentHour < 12) {
-            return  "¡Buenos días " + name + "!";
-        } else if (currentHour < 6 || currentHour >= 20) {
-            return  "¡Buenas noches " + name + "!";
-        } else {
-            return  "¡Buenas tardes " + name + "!";
+            return "¡Buenos días " + name + "!";
         }
+
+        if (currentHour < 6 || currentHour >= 20) {
+            return "¡Buenas noches " + name + "!";
+        }
+
+        return "¡Buenas tardes " + name + "!";
     }
 }
